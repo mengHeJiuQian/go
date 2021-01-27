498a4bb6b69533853a41072310286dab6874bb35
mengHeJiuQian <1197563121@qq.com>
2021-01-27 20:22:12 +0800

    提交历史代码

---
+++ hello-go/gin_demo/hello.gp.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+func main() {
+	router := gin.Default()
+	router.GET("/", func(context *gin.Context) {
+		context.String(http.StatusOK, "Hello World")
+	})
+	router.Run(":8080")
+}
